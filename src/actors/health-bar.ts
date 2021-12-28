ea4512eb350059ceba9592fa24255f8437801060
Erik Onarheim <erik.onarheim@gmail.com>
2021-12-28 12:57:20 -0600

    update font

--- src/actors/health-bar.ts
+++ src/actors/health-bar.ts
@@ -32,7 +32,7 @@ export class HealthBar extends ex.Actor {
         ctx.strokeStyle = this.color.toString();
         ctx.fillStyle = this.color.toString();
         ctx.lineWidth = 3;
-        ctx.font = 'normal 30px sans-serif'
+        ctx.font = 'normal 30px Open Sans'
         ctx.fillText("HP", 0, 30);
         ctx.strokeRect(0, 35, Config.healthBarWidth + 10, Config.healthBarHeight + 10);
         ctx.fillRect(5, 40, this.healthBarWidth, Config.healthBarHeight);
--- src/game.ts
+++ src/game.ts
@@ -30,6 +30,7 @@ export class Game extends ex.Scene {
         });
         scoreLabel.font.quality = 3;
         scoreLabel.fontSize = 30;
+        scoreLabel.fontFamily = "Open Sans";
         scoreLabel.transform.coordPlane = ex.CoordPlane.Screen;
         scoreLabel.color = ex.Color.Azure;
         scoreLabel.on('preupdate', function(this: ex.Label, evt){
