cca85c92b193eec58119435c472e6a89981577d1
Erik Onarheim <erik.onarheim@gmail.com>
2022-01-21 23:20:48 -0600

    Update test (remove devtool for test)

--- src/game.ts
+++ src/game.ts
@@ -7,10 +7,6 @@ import { Baddie } from './actors/baddie';
 import Config from './config';
 
 import { animManager } from './actors/animation-manager';
-
-
-import { DevTool } from '@excaliburjs/dev-tools';
-
 export class Game extends ex.Scene {
     random = new ex.Random(1337); // seeded random
 
@@ -19,9 +15,6 @@ export class Game extends ex.Scene {
     }
 
     onInitialize(engine: ex.Engine) {
-        const devtool = new DevTool(engine);
-        devtool.pane.expanded = false;
-
         engine.add(animManager);
 
         const ship = new Ship(engine.halfDrawWidth, 800, 80, 80);
